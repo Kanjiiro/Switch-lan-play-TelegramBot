ea1fbd0630e2ae5b5f7715caa423eff1dda4cb4f
Igor195 <igor195@live.it>
2018-11-12 21:34:11 +0100

    Add files via upload
    - Now you can switch between the servers.

--- bot.py
+++ bot.py
@@ -1,30 +1,41 @@
-import os
-import sys
-import time
-import telepot
-import subprocess
+#!/usr/bin/env python3
+
+import time, sys, telepot, subprocess
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from subprocess import Popen, PIPE
+
 
 
 TOKEN = ''
 message_with_inline_keyboard = None
-status= 0
-#choose form eth0/wlan0 or other interfaces 
-network_interface= eth0
+process= None
+
+#choose form eth0/wlan0 or other interfaces
+
+network_interface='eth0'
+
 def connect(server_ip):
+    global status
     global network_interface
-    os.system("sudo ./lan-play --relay-server-addr "+server_ip+" --netif "+network_interface+" &")
+    global connected
+    global process
+    if process != None:
+        os.system('sudo ip link set eth0 down')
+        os.system('sudo ip link set eth0 up')
+        os.system('sudo service network-manager restart')
+        time.sleep(4)
+        process = None
+    process = subprocess.Popen("sudo" + "  ./lan-play " + "--relay-server-addr " + server_ip + " --netif " + network_interface + " &", shell=True)
+
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print('Chat:', content_type, chat_type, chat_id)
 
     if content_type != 'text':
-        bot.sendMessage(from_id, text='Use /servers')
-        return 
+        bot.sendMessage(chat_id, text='Use /servers')
+        return
     if msg['text'] == "/start":
-        bot.sendMessage(from_id, text='Hi, write /servers to list the servers.')
+        bot.sendMessage(chat_id, text='Hi, write /servers to list the servers.')
     command = msg['text']
     if command == '/servers':
         markup = InlineKeyboardMarkup(inline_keyboard=[
@@ -49,59 +60,43 @@ def on_callback_query(msg):
     query_id, from_id, data = telepot.glance(msg, flavor='callback_query')
     print('Callback query:', query_id, from_id, data)
     global status
-    if status == 1:
-        status = 0
-        os.system('sudo reboot')
 
     if data == '1':
         bot.sendMessage(from_id, text='Connected to 35.236.10.223:11451 US 🇺🇸')
-        status=1
         connect('35.236.10.223:11451')
-
     elif data == '2':
         bot.sendMessage(from_id, text='Connected to nxlan.duckdns.org:11451 US 🇺🇸')
-        status=1
         connect('nxlan.duckdns.org:11451')
     elif data == '3':
         bot.sendMessage(from_id, text='Connected to switch.lan-play.com:11451 FR 🇫🇷')
-        status=1
         connect('switch.lan-play.com:11451')
     elif data == '4':
         bot.sendMessage(from_id, text='Connected to relay1.fadx.co.uk:11451 EU 🇬🇧')
-        status=1
         connect('relay1.fadx.co.uk:11451')
     elif data == '5':
         bot.sendMessage(from_id, text='Connected to relay2.fadx.co.uk:11451  EU 🇬🇧')
-        status=1
         connect('relay2.fadx.co.uk:11451')
     elif data == '6':
         bot.sendMessage(from_id, text='Connected to switch.glaciergaming.co.uk:11451 EU 🇬🇧')
-        status=1
         connect('switch.glaciergaming.co.uk:11451')
     elif data == '7':
         bot.sendMessage(from_id, text='Connected to switch.homelab.tech:11451 EU 🇬🇧')
-        status=1
         connect('switch.homelab.tech:11451')
     elif data == '8':
         bot.sendMessage(from_id, text='Connected to bandits.duckdns.org:11451 EU 🇪🇺')
-        status=1
         connect('bandits.duckdns.org:11451')
     elif data == '9':
         bot.sendMessage(from_id, text='Connected to memehouse.de:11451 EU 🇩🇪')
-        status=1
         connect('memehouse.de:11451')
     elif data == '10':
         bot.sendMessage(from_id, text='Connected to relay.it-cybergate.club:11451 EU 🇩🇪')
-        status=1
         connect('relay.it-cybergate.club:11451')
     elif data == '11':
         bot.sendMessage(from_id, text='Connected to slp.rush-hour.wo.tc:11451 KR 🇰🇷')
-        status=1
         connect('slp.rush-hour.wo.tc:11451')
     elif data == '12':
         bot.sendMessage(from_id, text='Connected to lithium2g.ddns.net:11451 AU 🇦🇺')
-        status=1
-        connect('lithium2g.ddns.net:11451')    
+        connect('lithium2g.ddns.net:11451')
 
 
 
